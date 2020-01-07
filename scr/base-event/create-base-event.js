f01a5a6ed68e8723c1dc4aef698f9a6df45b85e3
unknown <oleg.tsesko@gmail.com>
2020-01-07 14:25:54 +0600

    add 'create-base-event' function

---
+++ scr/base-event/create-base-event.js
@@ -0,0 +1,53 @@
+const admin = require('firebase-admin');
+const moment = require('moment-timezone');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
+
+const db = admin.firestore();
+
+exports.createBaseEvent = async (req, res) => {
+    let weekDays = req.body.weekDays;
+    let startDate = req.body.startDate;
+    let endDate = req.body.endDate;
+    let slotsCount = req.body.slotsCount;
+    let timezone = req.body.timezone;
+
+    let startMoment = moment.tz(startDate, timezone);
+    let endMoment = moment.tz(endDate, timezone)
+        .add(1, 'day'); // include endDate in diapason
+
+    let instances = [];
+
+    weekDays.forEach((weekDay, index) => {
+        if (weekDay) {
+            let weekDayNumber = index + 1;
+            let hhMm = weekDay.split(':');
+            let hours = hhMm[0];
+            let minutes = hhMm[1];
+
+            let tmpMoment = startMoment.clone()
+                .hours(hours)
+                .minutes(minutes)
+                .day(weekDayNumber);
+
+            if (tmpMoment.isAfter(startMoment, 'd')) {
+                instances.push(tmpMoment.unix());
+            }
+            while (tmpMoment.isBefore(endMoment)) {
+                tmpMoment.add(7, 'days');
+                instances.push(tmpMoment.unix());
+            }
+        }
+    })
+
+    let addedBaseEvent = await db.collection('base-events').add({
+        weekDays,
+        startDate,
+        endDate,
+        slotsCount,
+        instances
+    });
+    res.status(200).send(addedBaseEvent.id);
+};
